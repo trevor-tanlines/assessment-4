da52d3b981a78d2301b307060ac138dcf138ea8c
Trevor <ttan20@my.bcit.ca>
2024-03-24 22:06:38 -0500

    Changed the header, adjusted some stuff

--- components/Header/header.js
+++ components/Header/header.js
@@ -5,16 +5,22 @@ import styles from "@/components/Header/Header.module.css"
 export default function Header() {
     return (
         <div className={styles.header}>
-            <div className={styles.bcit}>
-                <img src="/images/bcit_logo.png" alt="bcit" width={45} height={45}></img>
+            <div className={styles.left}>
+                <div className={styles.bcit}>
+                    <img src="/images/bcit_logo.png" alt="bcit" width={45} height={45}></img>
+                </div>
+                <div className={styles.list}>
+                    <ul>
+                        <Link href="/">
+                            <li>Home</li>
+                        </Link>
+                        <Link href="/about"><li>About</li></Link>
+                        
+                        <Link href="/contact"><li>Contact Us</li></Link>
+                    </ul>
+                </div>
             </div>
-            <div className={styles.list}>
-                <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact Us</li>
-                </ul>
-            </div>
+
             <div className={styles.bars}>
                 <img src="/images/bars.png" alt="bar" width={40} height={15}></img>
             </div>
